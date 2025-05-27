b4f96b9788b04bd28c81185bbf109d4d3b95ae3f
Erdenebat <148745693+Erdenebat123@users.noreply.github.com>
2025-05-27 16:33:51 +0800

    llr

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv').config({ path: './config/config.env' })
 const colors = require('colors')
-const cors = require('cors')
 const Connectdb = require('./config/DB')
 const app = express()
 Connectdb()
-
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/', require('./Routes/authRoutes'))
+app.use('http://localhost:5173/', require('./Routes/authRoutes'))
 
 app.listen(
   process.env.PORT,
